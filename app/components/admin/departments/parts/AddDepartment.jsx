24b820fbb8c789c52fdffd0bc8cb2b10a16d5e88
jatin2040947 <jatin@mobyink.com>
2024-11-26 17:10:18 +0530

    m

--- app/components/admin/departments/parts/AddDepartment.jsx
+++ app/components/admin/departments/parts/AddDepartment.jsx
@@ -1,3 +1,96 @@
+// "use client";
+
+// import { InnerContainerHead } from "@/app/styledComponents/admin/AdminHead";
+// import {
+//   BlueButtonSmall,
+//   GreenButtonSmall,
+// } from "@/app/styledComponents/admin/Buttons";
+// import {
+//   InnerContainer,
+//   InnerContainerHeadSection,
+//   ModalContainer,
+// } from "@/app/styledComponents/admin/Container";
+// import {
+//   InputSection,
+//   Label,
+//   TextInput,
+// } from "@/app/styledComponents/admin/Inputs";
+// import { Box, Stack } from "@mui/material";
+// import { placeholder } from "jodit/esm/plugins/placeholder/placeholder";
+// import { useMemo, useRef, useState } from "react";
+// import dynamic from "next/dynamic";
+// const Jodit = dynamic(() => import("./Jodit"), { ssr: false });
+// const menu = [
+//   "Departments",
+//   "Doctors",
+//   "Appointments",
+//   "Academics",
+//   "Download Files",
+//   "Academics Notices",
+//   "Tpa Logo",
+//   "Events",
+//   "Testimonials",
+//   "Award Accreditation",
+//   "Enquiries",
+//   "Contact",
+//   "Videos",
+//   "Latest Openings",
+//   "Careers",
+//   "Health Plans",
+//   "Health Tips",
+// ];
+
+// const cms = ["Menus", "Pages", "Posts", "Templates", "Site Settings"];
+
+// const donations = ["Donation to", "Doners"];
+
+// export default function AddDepartment({ setViewForm }) {
+//   const editor = useRef(null);
+//   const [content, setContent] = useState("");
+//   const config = useMemo(
+//     () => ({
+//       readonly: false,
+//       placeholder: "Start typing...",
+//       language: "en", // Set default language
+//     }),
+//     []
+//   );
+
+//   return (
+//     <ModalContainer>
+//       <InnerContainer width={"80%"}>
+//         <InnerContainerHead>New User</InnerContainerHead>
+//         <InnerContainerHeadSection column>
+//           <InputSection>
+//             <Label>Name</Label>
+//             <TextInput placeholder="Enter Name" />
+//           </InputSection>
+//           <InputSection>
+//             <Label>Image</Label>
+//             <TextInput placeholder="Enter Username" type="file" />
+//           </InputSection>
+//           <InputSection>
+//             <Label>Banner Image</Label>
+//             <TextInput placeholder="Enter Password" type="file" />
+//           </InputSection>
+//           <InputSection>
+//             <Label>Content</Label>
+//              <Jodit content={content} setContent={setContent} />
+//           </InputSection>
+//           <InputSection>
+//             <Label></Label>
+//             <Stack direction={"row"} gap={"10px"}>
+//               <GreenButtonSmall>Save</GreenButtonSmall>
+//               <BlueButtonSmall onClick={() => setViewForm(false)}>
+//                 Cancel
+//               </BlueButtonSmall>
+//             </Stack>
+//           </InputSection>
+//         </InnerContainerHeadSection>
+//       </InnerContainer>
+//     </ModalContainer>
+//   );
+// }
 "use client";
 
 import { InnerContainerHead } from "@/app/styledComponents/admin/AdminHead";
@@ -15,46 +108,15 @@ import {
   Label,
   TextInput,
 } from "@/app/styledComponents/admin/Inputs";
-import { Box, Stack } from "@mui/material";
-import { placeholder } from "jodit/esm/plugins/placeholder/placeholder";
-import { useMemo, useRef, useState } from "react";
+import { useState } from "react";
+
+// Dynamically import Jodit component
 import dynamic from "next/dynamic";
+import { Stack } from "@mui/material";
 const Jodit = dynamic(() => import("./Jodit"), { ssr: false });
-const menu = [
-  "Departments",
-  "Doctors",
-  "Appointments",
-  "Academics",
-  "Download Files",
-  "Academics Notices",
-  "Tpa Logo",
-  "Events",
-  "Testimonials",
-  "Award Accreditation",
-  "Enquiries",
-  "Contact",
-  "Videos",
-  "Latest Openings",
-  "Careers",
-  "Health Plans",
-  "Health Tips",
-];
-
-const cms = ["Menus", "Pages", "Posts", "Templates", "Site Settings"];
-
-const donations = ["Donation to", "Doners"];
 
 export default function AddDepartment({ setViewForm }) {
-  const editor = useRef(null);
   const [content, setContent] = useState("");
-  const config = useMemo(
-    () => ({
-      readonly: false,
-      placeholder: "Start typing...",
-      language: "en", // Set default language
-    }),
-    []
-  );
 
   return (
     <ModalContainer>
@@ -75,6 +137,7 @@ export default function AddDepartment({ setViewForm }) {
           </InputSection>
           <InputSection>
             <Label>Content</Label>
+            {/* Use Jodit with dynamic import */}
             <Jodit content={content} setContent={setContent} />
           </InputSection>
           <InputSection>
