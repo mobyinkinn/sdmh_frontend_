ee7c98846a08110dc00a7d39826ee3b74ce7142e
NikhilSharma-007 <niks21902@gmail.com>
2025-02-01 18:01:53 +0530

    ui fixes of health check up, details, acdemics deanery , library, notices and sonc

--- app/components/Frontend/academicsCons/parts/PhysicalSetup.jsx
+++ app/components/Frontend/academicsCons/parts/PhysicalSetup.jsx
@@ -1,15 +1,23 @@
-import { Head1 } from '@/app/styledComponents/frontend/Headings'
-import { ParaNormal } from '@/app/styledComponents/frontend/Para';
-import { Stack } from '@mui/material'
-import React from 'react'
+import { Head1 } from "@/app/styledComponents/frontend/Headings";
+import { ParaNormal } from "@/app/styledComponents/frontend/Para";
+import { Stack } from "@mui/material";
+import React from "react";
 
 const PhysicalSetup = () => {
   return (
-    <Stack bgcolor={"#007946"} p={5} gap={1}>
-      <Head1 style={{ color: "white" }}>Physical Set-up</Head1>
+    <Stack bgcolor={"#007946"} p={{ xs: 3, md: 5 }} gap={1}>
+      <Head1
+        fontSize={{ sm: "1.5rem", smm: "2rem", md: "2.5rem", lg: "3.5rem" }}
+        style={{ color: "white" }}
+      >
+        Physical Set-up
+      </Head1>
       <Stack gap={3}>
         <Stack bgcolor={"#02814B"} p={2} borderRadius={2}>
-          <ParaNormal style={{ textAlign: "center", color: "White" }}>
+          <ParaNormal
+            fontSize={{ sm: "15px", smm: "17px", md: "20px", lg: "25px" }}
+            style={{ textAlign: "center", color: "White" }}
+          >
             The school is housed in a separate building which was inaugurated by
             the then Chief Minister of Rajasthan, Shri Bhairon Singh Shekhawat
             on 9th December 1995. The school is well equipped and well furnished
@@ -18,7 +26,10 @@ const PhysicalSetup = () => {
           </ParaNormal>
         </Stack>
         <Stack bgcolor={"#02814B"} p={1} borderRadius={2}>
-          <ParaNormal style={{ textAlign: "center", color: "White" }}>
+          <ParaNormal
+            fontSize={{ sm: "15px", smm: "17px", md: "20px", lg: "25px" }}
+            style={{ textAlign: "center", color: "White" }}
+          >
             A student hostel is attached to the school building. In
             acknowledgement of the generous help and support from Shri Ganesh
             Rana, the school was re-named as “SDMH Chand Behari Sita Devi Rana
@@ -28,6 +39,6 @@ const PhysicalSetup = () => {
       </Stack>
     </Stack>
   );
-}
+};
 
-export default PhysicalSetup
+export default PhysicalSetup;
