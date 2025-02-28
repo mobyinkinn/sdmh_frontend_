089385992688bb213a0b8d7a0ec3df6afe9dfaba
jatin2040947 <jatin@mobyink.com>
2025-02-28 11:45:36 +0530

    asd

--- app/components/Frontend/coe/parts/components/DetailsHero.jsx
+++ app/components/Frontend/coe/parts/components/DetailsHero.jsx
@@ -48,6 +48,71 @@
 //     </Stack>
 //   );
 // }
+
+
+// "use client";
+// import { Head1 } from "@/app/styledComponents/frontend/Headings";
+// import { Box, Stack } from "@mui/material";
+// import baloon from "../assets/bigHotAirBaloon.png";
+// import { ParaNormal } from "@/app/styledComponents/frontend/Para";
+// import Spinner from "@/app/components/ui/Spinner";
+// import { useEffect, useState } from "react";
+
+// export default function DetailsHero({ data, isLoading }) {
+//   const [paragraphs, setParagraphs] = useState([]);
+
+//   useEffect(() => {
+//     if (typeof window === "undefined" || !data?.content) return;
+
+//     const extractParagraphs = (htmlContent) => {
+//       const parser = new DOMParser();
+//       const doc = parser.parseFromString(htmlContent, "text/html");
+//       const paragraphs = doc.querySelectorAll("p");
+
+//       // Convert NodeList to an array of text content
+//       return Array.from(paragraphs).map((item) => item.textContent.trim());
+//     };
+
+//     setParagraphs(extractParagraphs(data?.content));
+//   }, [data]);
+
+//   console.log("Extracted Paragraphs c1:", paragraphs);
+
+//   if (isLoading) {
+//     return <Spinner />;
+//   }
+
+//   return (
+//     <Stack>
+//       <Head1 textAlign="left" color="black">
+//         {data?.name}
+//       </Head1>
+//       <Head1 textAlign="left" color="black">
+//         Best for Your Business?
+//       </Head1>
+//       <Box
+//         sx={{
+//           backgroundImage: `url(${data?.bannerImage})`,
+//           backgroundSize: "cover",
+//           backgroundPosition: "center center",
+//           width: "100%",
+//           height: "70vh",
+//           margin: "30px 0",
+//         }}
+//       ></Box>
+//       <Stack gap={"20px"}>
+//         {paragraphs.map((el, i) => (
+//           <ParaNormal key={i} textAlign={"justify"}>
+//             {el}
+//           </ParaNormal>
+//         ))}
+//       </Stack>
+//     </Stack>
+//   );
+// }
+
+
+
 "use client";
 import { Head1 } from "@/app/styledComponents/frontend/Headings";
 import { Box, Stack } from "@mui/material";
@@ -55,27 +120,8 @@ import baloon from "../assets/bigHotAirBaloon.png";
 import { ParaNormal } from "@/app/styledComponents/frontend/Para";
 import Spinner from "@/app/components/ui/Spinner";
 import { useEffect, useState } from "react";
-
 export default function DetailsHero({ data, isLoading }) {
-  const [paragraphs, setParagraphs] = useState([]);
-
-  useEffect(() => {
-    if (typeof window === "undefined" || !data?.content) return;
-
-    const extractParagraphs = (htmlContent) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(htmlContent, "text/html");
-      const paragraphs = doc.querySelectorAll("p");
-
-      // Convert NodeList to an array of text content
-      return Array.from(paragraphs).map((item) => item.textContent.trim());
-    };
-
-    setParagraphs(extractParagraphs(data?.content));
-  }, [data]);
-
-  console.log("Extracted Paragraphs c1:", paragraphs);
-
+  
   if (isLoading) {
     return <Spinner />;
   }
@@ -98,13 +144,12 @@ export default function DetailsHero({ data, isLoading }) {
           margin: "30px 0",
         }}
       ></Box>
-      <Stack gap={"20px"}>
-        {paragraphs.map((el, i) => (
-          <ParaNormal key={i} textAlign={"justify"}>
-            {el}
-          </ParaNormal>
-        ))}
-      </Stack>
+      <div
+        className="set"
+        dangerouslySetInnerHTML={{
+          __html: data.content,
+        }}
+      ></div>
     </Stack>
   );
 }
