8f34b31890a1bb1d836905e2f341f67082a2c438
jatin2040947 <jatin@mobyink.com>
2025-03-18 17:21:29 +0530

    asd

--- app/components/services/Api.js
+++ app/components/services/Api.js
@@ -1,4 +1,4 @@
-export const ApiUrl = "http://165.232.177.47/api/v1";
+export const ApiUrl = "https://165.232.177.47/api/v1";
 
 //https://sdmh-backend-2.onrender.com/api/v1
 //http://localhost:8000/api/v1
