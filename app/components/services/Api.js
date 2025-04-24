03fa04f7cd860f558c1031ad518198153464c0b8
NikhilSharma-007 <niks21902@gmail.com>
2025-04-24 13:19:21 +0530

    changes

--- app/components/services/Api.js
+++ app/components/services/Api.js
@@ -1,4 +1,4 @@
-export const ApiUrl = "http://213.210.21.16/api/v1";
+export const ApiUrl = "https://appsita.com/api/v1";
 
 //https://sdmh-backend-2.onrender.com/api/v1
 //http://localhost:8000/api/v1
