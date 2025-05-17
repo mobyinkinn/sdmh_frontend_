9c280ea5dc8465d11d3617eb7792067b50f9439f
root <root@srv792785.hstgr.cloud>
2025-05-17 02:43:48 +0000

    asd

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -14,11 +14,14 @@ export const dynamic = "force-dynamic"; // Optional: remove this if not needed
 // ✅ Hardcoded metadata
 export async function generateMetadata() {
   return {
-    title: "Santokba Durlabji Memorial Hospital - SDMH",
+    title: "Santokba Durlabhji Memorial Hospital - SDMH",
     description:
       "Trusted multispeciality hospital offering advanced medical care with compassion.",
     keywords:
       "SDMH, Jaipur hospital, best hospital in Rajasthan, multispeciality healthcare",
+ other: {
+      "google-site-verification": "QTcmfuX31VXilPv-HlbFdsiCkAOoSPCKra2Tz-y9CtM",
+    },
   };
 }
 
